130dfad4d3f887695b0369018112048ceb9c2c01
Apple8819 <appletea8819@gmail.com>
2021-09-24 11:01:49 +0900

    SampleController.javaからtest.htmlに文字列を渡すように修正。test.htmlではThymeleafを使用して受け取った文字列を表示するように修正。

--- Hello/src/main/java/app/SampleController.java
+++ Hello/src/main/java/app/SampleController.java
@@ -2,13 +2,13 @@ package app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SampleController {
-	@RequestMapping(value = "/sample/test") //http://localhost:8080/sample/test
-	private String hello() {
+	@GetMapping(value = "/sample/test") //http://localhost:8080/sample/test
+	private String hello(Model model) {
+		model.addAttribute("message", "Hello World");
 		return "test";
 	}
 }
